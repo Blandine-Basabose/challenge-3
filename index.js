8758eafac14bb707d8a59f77d4888b623d0aa30f
Blandine-Basabose <blandineusanase@gmail.com>
2021-08-26 23:36:33 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const email = document.querySelector('.email')
+const ctaBtn = document.querySelector('.cta')
+const emailError = document.querySelector('.error-message')
+
+ctaBtn.onclick = function () {
+    if (email.value === "") {
+        email.style.borderColor = "red"
+      
+        emailError.textContent = 'Your email is empty'
+        emailError.style.color = "red"
+        return false
+    } else if (!(email.value.includes("@"))) {
+        email.style.borderColor = "#"
+        emailError.textContent = 'Check your email please'
+        emailError.style.color = "red"
+        return false
+    }
+      // trying to work on valid email properties...learning on regrex expression 
+    email.style.borderColor = "#ccc"
+    emailError.textContent = email.value
+    emailError.textContent ="You are good to go!!"
+    emailError.style.color = "green"
+    email.value = ""
+}
